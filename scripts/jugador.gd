6be6065c26d823b0c985b484f54a9edbddd27687
lulorojas <lulorojas11@gmail.com>
2026-09-14 16:45:34 -0300

    Suma ataque a distancia y golpe con direccion segun movimiento
    El golpe ahora sale hacia el lado que mira el personaje (4 direcciones,
    top down) y se agrega un proyectil que se dispara con una accion nueva.

--- scripts/jugador.gd
+++ scripts/jugador.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
+const Proyectil = preload("res://escenas/proyectil.tscn")
+
 @export var velocidad = 200.0
 @export var duracion_golpe = 0.3
+@export var distancia_golpe = 24.0
+@export var distancia_disparo = 20.0
 
-var mirando_derecha = true
+var mirando = "abajo"
 var golpeando = false
 
 @onready var zona_golpe = $ZonaGolpe
@@ -20,24 +24,46 @@ func _physics_process(delta):
 	entrada.y = Input.get_axis("mover_arriba", "mover_abajo")
 	entrada = entrada.normalized()
 
-	if entrada.x > 0:
-		mirando_derecha = true
-	elif entrada.x < 0:
-		mirando_derecha = false
+	if entrada.x != 0 or entrada.y != 0:
+		if abs(entrada.x) > abs(entrada.y):
+			mirando = "derecha" if entrada.x > 0 else "izquierda"
+		else:
+			mirando = "abajo" if entrada.y > 0 else "arriba"
 
-	$Sprite2D.flip_h = not mirando_derecha
-	zona_golpe.position.x = abs(zona_golpe.position.x) * (1 if mirando_derecha else -1)
+	$Sprite2D.flip_h = mirando == "izquierda"
 
 	velocity = entrada * velocidad
 	move_and_slide()
 
 	if Input.is_action_just_pressed("golpear"):
 		golpear()
+	elif Input.is_action_just_pressed("disparar"):
+		disparar()
+
+func direccion_mirando():
+	match mirando:
+		"arriba":
+			return Vector2.UP
+		"abajo":
+			return Vector2.DOWN
+		"izquierda":
+			return Vector2.LEFT
+		_:
+			return Vector2.RIGHT
 
 func golpear():
 	golpeando = true
+	zona_golpe.position = direccion_mirando() * distancia_golpe
 	zona_golpe.monitoring = true
 	temporizador_golpe.start(duracion_golpe)
+
+func disparar():
+	golpeando = true
+	temporizador_golpe.start(duracion_golpe)
+	var proyectil = Proyectil.instantiate()
+	get_parent().add_child(proyectil)
+	proyectil.global_position = global_position + direccion_mirando() * distancia_disparo
+	proyectil.direccion = direccion_mirando()
 
 func _on_temporizador_golpe_timeout():
 	golpeando = false
